bf998aad1081afce1109726278254518c86be069
Meagle <mindfulyeon@gmail.com>
2020-03-16 11:53:42 +0900

    store, reducer, dispatch 추가

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
+import { createStore } from "redux";
+
 const number = document.querySelector("span");
 
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+const ADD = "ADD";
+const MINUS = "MINUS";
+
+// reducer : 스테이트가 존재하지 않는다면 0으로 스테이트를 초기화함
+const countModifier = (count = 0, action) => {
+  console.log(count, action);
+  if (action.type === ADD) {
+    return count + 1;
+  } else if (action.type === MINUS) {
+    return count - 1;
+  } else {
+    return count;
+  }
+};
+
+// store
+const countStore = createStore(countModifier);
+
+countStore.dispatch({ type: ADD });
+countStore.dispatch({ type: ADD });
+countStore.dispatch({ type: ADD });
+countStore.dispatch({ type: ADD });
+countStore.dispatch({ type: ADD });
+countStore.dispatch({ type: ADD });
+countStore.dispatch({ type: MINUS });
+
+console.log(countStore.getState());
