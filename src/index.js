5e74dcdf628f58b00fbb883c021f4e77404c6345
Meagle <mindfulyeon@gmail.com>
2020-03-18 16:12:31 +0900

    Todo  리스트 추가, 아직 save 불가

--- src/index.js
+++ src/index.js
@@ -1,47 +1,19 @@
 import { createStore } from "redux";
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-const add = document.getElementById("add");
-const number = document.querySelector("span");
-const minus = document.getElementById("minus");
-
-number.innerText = 0;
-
-const ADD = "ADD";
-const MINUS = "MINUS";
-
-// reducer : 스테이트가 존재하지 않는다면 0으로 스테이트를 초기화함
-const countModifier = (count = 0, action) => {
-  switch (action.type) {
-    case ADD:
-      return count + 1;
-
-    case MINUS:
-      return count - 1;
-
-    default:
-      return count;
-  }
+const createTodo = toDo => {
+  const li = document.createElement("li");
+  li.innerText = toDo;
+  ul.appendChild(li);
 };
 
-// store
-const countStore = createStore(countModifier);
-
-// subscribe
-const onChange = () => {
-  number.innerText = countStore.getState();
+const onSubmit = e => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  createTodo(toDo);
 };
 
-// 스테이트가 스토어에서 바뀔때마다 onChange 함수 호출됨
-countStore.subscribe(onChange);
-
-// action creator
-const handleAdd = () => {
-  countStore.dispatch({ type: ADD });
-};
-
-const handleMinus = () => {
-  countStore.dispatch({ type: MINUS });
-};
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+form.addEventListener("submit", onSubmit);
