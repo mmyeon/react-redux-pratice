06d0c1c5912985e96890a97afc86c0582ca1a83e
Meagle <mindfulyeon@gmail.com>
2020-03-18 17:40:11 +0900

    todo 삭제기능 추가 2

--- src/index.js
+++ src/index.js
@@ -26,9 +26,11 @@ const reducer = (state = [], action) => {
   // console.log(action);
   switch (action.type) {
     case ADD_TODO:
-      return [{ text: action.text, id: Date.now() }, ...state];
+      const newToDoObj = { text: action.text, id: Date.now() };
+      return [newToDoObj, ...state];
     case DELETE_TODO:
-      return [];
+      const cleaned = state.filter(toDo => toDo.id !== action.id);
+      return cleaned;
     default:
       return state;
   }
@@ -39,29 +41,33 @@ const store = createStore(reducer);
 
 store.subscribe(() => console.log(store.getState()));
 
+// dispatch action
 const dispatchAddToDo = text => {
   store.dispatch(addToDo(text));
 };
 
 const dispatchDeleteToDo = e => {
-  const id = e.target.parentNode.id;
-  console.log("id : ", id);
+  // 이벤트 타겟 확인
+  // console.log(e.target.parentNode);
+  const id = parseInt(e.target.parentNode.id);
+  // console.log("id", id);
   store.dispatch(deleteToDo(id));
 };
 
 const paintToDos = () => {
   const toDos = store.getState();
-  console.log(toDos);
-  ul.innerHTML = "";
+  ul.innerText = "";
+  // console.log("ul : ", ul);
   toDos.forEach(toDo => {
     const li = document.createElement("li");
     const btn = document.createElement("button");
+    btn.innerText = "DEL";
+    btn.addEventListener("click", dispatchDeleteToDo);
     li.id = toDo.id;
     li.innerText = toDo.text;
-    btn.innerText = "DEL";
-    btn.addEventListener("click", deleteToDo);
-    ul.appendChild(li);
+    console.log("li : ", li);
     li.appendChild(btn);
+    ul.appendChild(li);
   });
 };
 
